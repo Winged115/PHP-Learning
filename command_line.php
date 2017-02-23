2f466f153b905c871ccfcfca2c9ba08f7e9f3b36
Jack Noble <jack.carmco@yahoo.com>
2017-02-23 14:54:16 -0600

    Test commandline file

---
+++ command_line.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Command Line PHP App</title>
+  </head>
+  <body>
+    <?php
+      echo "Test";
+     ?>
+  </body>
+</html>
