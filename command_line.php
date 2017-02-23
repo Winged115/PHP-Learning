946fc66547f766c9b9bf95a79dcfcab456ae84ec
Jack Noble <jack.carmco@yahoo.com>
2017-02-23 15:30:32 -0600

    modify for small edge case of only one argument

--- command_line.php
+++ command_line.php
@@ -1,6 +1,9 @@
 <?php
+  if ($argc === 1) {
+  print "$argc argument was passed. It was: \n";
+} else {
   print "$argc arguments were passed. In order: \n";
-
+}
   for ($i = 0; $i <= $argc - 1; ++$i) {
     print "$i: $argv[$i]\n";
   }
