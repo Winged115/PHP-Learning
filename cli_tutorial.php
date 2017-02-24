93988e1e758893ca8e398dd64bdaebccedd09709
Jack Noble <jack.carmco@yahoo.com>
2017-02-23 18:10:58 -0600

    Take input from a user

--- cli_tutorial.php
+++ cli_tutorial.php
@@ -1,4 +1,18 @@
 <?php
+  echo "Are you sure you want to do this? Type 'yes' to continue";
+
+  $handle = fopen("php://stdin", "r");
+  $line = fgets($handle);
+
+  if(trim($line) != "yes") {
+    echo "ABORTING!\n";
+    exit;
+  }
+
+  echo "\n";
+  echo "Thank you, continuing...\n";
+
+  /*
   if ($argc > 1) {
     for ($i = 0; $i <= $argc - 1; ++$i) {
       if ($i === $argc - 1) {
@@ -9,4 +23,4 @@
     }
 
   }
- ?>
+ */?>
