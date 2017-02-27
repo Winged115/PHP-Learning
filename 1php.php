87b407a9c423e028fc27b19a0035617bae097278
Jack Noble <jack.carmco@yahoo.com>
2017-02-27 17:34:44 -0600

    finish script challenges

--- 1php.php
+++ 1php.php
@@ -1,4 +1,21 @@
 <?php
-  //phpinfo();
-  
- ?>
+  $var = 'PHP Tutorial';
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h3>
+      <?php
+        //phpinfo();
+        //echo "Tomorrow I'll learn PHP global variables.\n";
+        //echo "This is a bad command : del c:\\*.*";
+        echo $var;
+      ?>
+    </h3>
+
+  </body>
+</html>
